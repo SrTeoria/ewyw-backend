5c9d955b35700a41a851bb7c4e28e60b22d71efc
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2021-06-27 11:31:25 -0500

    Se agrego una validacion de autorizacion a algunas rutas del restaurante, asi como una nueva ruta

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
-const { list, update, getRestaurant } = require('../controllers/restaurant.controller')
+const { list, update, getRestaurant, getRestaurantPublic } = require('../controllers/restaurant.controller')
+const { auth } = require('../utils/auth')
 
 router.route('/').get(list)
-router.route('/restaurant').get(getRestaurant)
-router.route('restaurantprofile').put(update)
+router.route('/restaurant').get(auth, getRestaurant)
+router.route('/restaurant/:restaurantId').get(auth, getRestaurantPublic)
+router.route('/restaurantprofile').put(auth, update)
 
 module.exports = router
