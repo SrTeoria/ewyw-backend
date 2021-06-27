7daedf83ad8c756f670434a485494b141811f39c
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2021-06-27 11:27:40 -0500

    Se agrego una validacion de autorizacion para el cliente

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const { list, update, getClient } = require('../controllers/client.controller')
+const { auth } = require("../utils/auth")
 
 router.route('/').get(list)
-router.route('/client').get(getClient)
-router.route('clientprofile').put(update)
+router.route('/client').get(auth, getClient)
+router.route('/clientprofile').put(auth, update)
 
 module.exports = router
