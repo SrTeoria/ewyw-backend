c8d103cc4bcc2c6738a23d65a26db117eddb929a
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2021-06-27 11:24:55 -0500

    Se elimino el modelo, ruta y controlador de las bebidas

--- src/routes/drink.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { list, update, createDrink } = require('../controllers/drink.controller')
-
-router.route('/createDrink').post(createDrink)
-router.route('/:drinkId').put(update)
-router.route('/').get(list)
-
-
-module.exports = router
